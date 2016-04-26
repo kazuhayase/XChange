e6a07683415adca4788a4123f9f04438db1dfd8d
ak <ak@cryptobitex.com>
2016-04-26 20:08:58 +0300

    httpReadTimeout and httpConnTimeout can be configured for bitfinex

--- xchange-bitfinex/src/main/java/org/knowm/xchange/bitfinex/v1/service/polling/BitfinexBasePollingService.java
+++ xchange-bitfinex/src/main/java/org/knowm/xchange/bitfinex/v1/service/polling/BitfinexBasePollingService.java
@@ -1,9 +1,5 @@
 package org.knowm.xchange.bitfinex.v1.service.polling;
 
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-
 import org.knowm.xchange.Exchange;
 import org.knowm.xchange.bitfinex.v1.BitfinexAdapters;
 import org.knowm.xchange.bitfinex.v1.BitfinexAuthenticated;
@@ -12,9 +8,13 @@ import org.knowm.xchange.bitfinex.v1.service.BitfinexPayloadDigest;
 import org.knowm.xchange.currency.CurrencyPair;
 import org.knowm.xchange.service.BaseExchangeService;
 import org.knowm.xchange.service.polling.BasePollingService;
-
+import si.mazi.rescu.ClientConfig;
 import si.mazi.rescu.ParamsDigest;
 import si.mazi.rescu.RestProxyFactory;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class BitfinexBasePollingService extends BaseExchangeService implements BasePollingService {
 
@@ -22,6 +22,9 @@ public class BitfinexBasePollingService extends BaseExchangeService implements B
   protected final BitfinexAuthenticated bitfinex;
   protected final ParamsDigest signatureCreator;
   protected final ParamsDigest payloadCreator;
+
+  public final static String HTTP_READ_TIMEOUT = "HTTP_READ_TIMEOUT";
+  public final static String HTTP_CONN_TIMEOUT = "HTTP_CONN_TIMEOUT";
 
   /**
    * Constructor
@@ -31,8 +34,24 @@ public class BitfinexBasePollingService extends BaseExchangeService implements B
   public BitfinexBasePollingService(Exchange exchange) {
 
     super(exchange);
-
-    this.bitfinex = RestProxyFactory.createProxy(BitfinexAuthenticated.class, exchange.getExchangeSpecification().getSslUri());
+    ClientConfig config = new ClientConfig();
+    Object readTimeout = exchange.getExchangeSpecification().getParameter(HTTP_READ_TIMEOUT);
+    if (readTimeout != null) {
+      try {
+        int timeoutValue = Integer.parseInt(readTimeout.toString());
+        config.setHttpReadTimeout(timeoutValue);
+      } catch (NumberFormatException e) {
+      }
+    }
+    Object connectTimeout = exchange.getExchangeSpecification().getParameter(HTTP_CONN_TIMEOUT);
+    if (connectTimeout != null) {
+      try {
+        int timeoutValue = Integer.parseInt(connectTimeout.toString());
+        config.setHttpConnTimeout(timeoutValue);
+      } catch (NumberFormatException e) {
+      }
+    }
+    this.bitfinex = RestProxyFactory.createProxy(BitfinexAuthenticated.class, exchange.getExchangeSpecification().getSslUri(), config);
     this.apiKey = exchange.getExchangeSpecification().getApiKey();
     this.signatureCreator = BitfinexHmacPostBodyDigest.createInstance(exchange.getExchangeSpecification().getSecretKey());
     this.payloadCreator = new BitfinexPayloadDigest();
